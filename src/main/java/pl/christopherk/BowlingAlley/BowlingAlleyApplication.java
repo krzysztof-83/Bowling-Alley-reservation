754b2d1becbe4e01f11de03dc35c9218c599b871
krzysztof-83 <gilduath@o2.pl>
2020-05-07 13:12:10 +0200

    Initialized project

---
+++ src/main/java/pl/christopherk/BowlingAlley/BowlingAlleyApplication.java
@@ -0,0 +1,13 @@
+package pl.christopherk.BowlingAlley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingAlleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingAlleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/christopherk/BowlingAlley/BowlingAlleyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.christopherk.BowlingAlley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingAlleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
